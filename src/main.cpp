47bf8e8ff648121bd6912f85616ddacd7b58f391
Todd Matthews <gtmatthews@gmail.com>
2024-10-25 15:33:34 -0400

    organize code

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,9 @@ constexpr float block_height{20.f};
 constexpr int count_blocks_x{11};
 constexpr int count_blocks_y{4};
 
+constexpr float ft_step{1.f};
+constexpr float ft_slice{1.f};
+
 using FrameTime = float;
 struct Rectangle
 {
@@ -30,23 +33,21 @@ struct Rectangle
 
 struct Brick : public Rectangle
 {
-    sf::RectangleShape shape{};
     bool is_destroyed{false};
-    Brick(float m_x, float m_y)
+    Brick(const float m_x, const float m_y)
     {
         shape.setPosition(m_x, m_y);
         shape.setSize({block_width, block_height});
         shape.setFillColor(sf::Color::Yellow);
-        shape.setOrigin(block_width / 2, block_height / 2);
+        shape.setOrigin(block_width / 2.f, block_height / 2.f);
     }
 };
-
 
 struct Ball
 {
     sf::CircleShape shape{};
     sf::Vector2f velocity{0.f, 0.f};
-    bool begin_phase{true};
+    bool reset{true};
     Ball(const float mX, const float mY)
     {
         shape.setPosition(mX, mY);
@@ -54,17 +55,22 @@ struct Ball
         shape.setFillColor(sf::Color::Red);
         shape.setOrigin(ball_radius, ball_radius);
     }
-    void update(Frametime m_FT)
+    void update(const FrameTime m_FT)
     {
         shape.move(velocity * m_FT);
-        if (left() < 0)
-            velocity.x = ball_velocity;
-        if (right() > window_width)
-            velocity.x = -ball_velocity;
-        if (top() < 0)
-            velocity.y = ball_velocity;
-        if (bottom() > window_height)
-            begin_phase = true;
+        if (reset)
+            velocity = {0.f, 0.f};
+        else
+        {
+            if (left() < 0)
+                velocity.x = ball_velocity;
+            else if (right() > window_width)
+                velocity.x = -ball_velocity;
+            else if (top() < 0)
+                velocity.y = ball_velocity;
+            else if (bottom() > window_height)
+                reset = true;
+        }
     }
 
     float x() const noexcept { return shape.getPosition().x; }
@@ -77,7 +83,6 @@ struct Ball
 
 struct Paddle : public Rectangle
 {
-    sf::RectangleShape shape{};
     sf::Vector2f velocity{};
 
     Paddle(const float mX, const float mY)
@@ -88,18 +93,14 @@ struct Paddle : public Rectangle
         shape.setOrigin(paddle_width / 2.f, paddle_height / 2.f);
     }
 
-    void update()
+    void update(const FrameTime m_FT)
     {
-        shape.move(velocity);
-        if ((sf::Keyboard::isKeyPressed(sf::Keyboard::Left) && left() > 0 ||
-             sf::Keyboard::isKeyPressed(sf::Keyboard::A)) &&
-            left() > 0)
+        shape.move(velocity * m_FT);
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) && left() > 0)
         {
             velocity.x = -paddle_velocity;
         }
-        else if ((sf::Keyboard::isKeyPressed(sf::Keyboard::Right) && right() < window_width ||
-                  sf::Keyboard::isKeyPressed(sf::Keyboard::D)) &&
-                 right() < window_width)
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) && right() < window_width)
         {
             velocity.x = paddle_velocity;
         }
@@ -140,11 +141,10 @@ void testCollision(Brick &m_brick, Ball &m_ball)
     const float overlap_left{m_ball.right() - m_brick.left()};
     const float overlap_right{m_brick.right() - m_ball.left()};
     const float overlap_top{m_ball.bottom() - m_brick.top()};
-    const float overlap_bottom{m_ball.bottom() - m_brick.top()};
+    const float overlap_bottom{m_brick.bottom() - m_ball.top()};
 
-    const bool ballFromLeft(abs(overlap_left < abs(overlap_right)));
-
-    const bool ballFromTop(abs(overlap_top < abs(overlap_bottom)));
+    const bool ballFromLeft(abs(overlap_left) < abs(overlap_right));
+    const bool ballFromTop(abs(overlap_top) < abs(overlap_bottom));
 
     const float min_overlap_x{ballFromLeft ? overlap_left : overlap_right};
     // ReSharper disable once CppTooWideScopeInitStatement
@@ -156,32 +156,61 @@ void testCollision(Brick &m_brick, Ball &m_ball)
         m_ball.velocity.y = ballFromTop ? -ball_velocity : ball_velocity;
     }
 }
-int main()
+
+struct Game
 {
+    sf::RenderWindow window{{window_width, window_height}, "Pipanoid"};
+    FrameTime lastFT{0.f};
+    FrameTime current_slice{0.f};
     sf::Font myFont;
-    if (!myFont.loadFromFile("fonts/JetBrainsMono-Regular.ttf"))
-    {
-        std::cerr << "Could not load font file" << '\n';
-    }
-    Ball ball{static_cast<float>(window_width) / 2.f, static_cast<float>(window_height) - 70.f};
+    sf::Text score_text{};
+    int score{0};
+    Ball ball{static_cast<float>(window_width) / 2.f, static_cast<float>(window_height) - 100.f};
     Paddle paddle{static_cast<float>(window_width) / 2.f, static_cast<float>(window_height) - 50.f};
     std::vector<Brick> bricks{};
-    for (int i = 0; i < count_blocks_x; ++i)
+
+    Game()
     {
-        for (int j = 0; j < count_blocks_y; ++j)
+        if (!myFont.loadFromFile("fonts/JetBrainsMono-Regular.ttf"))
         {
-            bricks.emplace_back((static_cast<float>(i) + 1.f) * (block_width + 3) + 22,
-                                (static_cast<float>(j) + 2.f) * (block_height + 3));
+            std::cerr << "Could not load font file" << '\n';
+        }
+        window.setFramerateLimit(60);
+        for (int i = 0; i < count_blocks_x; ++i)
+        {
+            for (int j = 0; j < count_blocks_y; ++j)
+            {
+                bricks.emplace_back((static_cast<float>(i) + 1.f) * (block_width + 3) + 22,
+                                    (static_cast<float>(j) + 2.f) * (block_height + 3));
+            }
         }
     }
-    sf::RenderWindow window(sf::VideoMode(window_width, window_height), "Bally Ball");
-    // window.setFramerateLimit(60);
-    int score = 44;
-    while (window.isOpen())
+    void run()
     {
-        // start our time interval
-        auto time_point_1 = std::chrono::high_resolution_clock::now();
+        while (window.isOpen())
+        {
+            // start our time interval
+            auto time_point_1(std::chrono::high_resolution_clock::now());
 
+            window.clear(sf::Color::Black);
+
+            inputPhase();
+            updatePhase();
+            drawPhase();
+            auto time_point_2(std::chrono::high_resolution_clock::now());
+            auto time_elapsed(time_point_2 - time_point_1);
+            const FrameTime ft{
+                    std::chrono::duration_cast<std::chrono::duration<float, std::milli>>(time_elapsed).count()};
+            lastFT = ft;
+            const auto ft_in_seconds(ft / 1000.f);
+            const auto fps(1.f / ft_in_seconds);
+
+            window.setTitle("FT: " + std::to_string(ft) + "\tPFS: " + std::to_string(fps));
+        }
+    }
+
+    void inputPhase()
+    {
         sf::Event event{};
         while (window.pollEvent(event))
         {
@@ -191,7 +220,7 @@ int main()
                     window.close();
                 if (event.key.code == sf::Keyboard::Space)
                 {
-                    ball.begin_phase = false;
+                    ball.reset = false;
                 }
             }
             if (event.type == sf::Event::Closed)
@@ -200,60 +229,60 @@ int main()
                 break;
             }
         }
-        if (ball.begin_phase)
-        {
-            ball.velocity.x = 0;
-            ball.shape.setPosition(paddle.x(), paddle.y() - 12.f);
-        }
-        ball.update();
-        paddle.update();
-        testCollision(paddle, ball);
+    }
 
-        for (auto &brick: bricks)
+    void updatePhase()
+    {
+        current_slice += lastFT;
+        for (; current_slice >= ft_slice; current_slice -= ft_slice)
         {
-            testCollision(brick, ball);
-        }
-        bricks.erase(std::ranges::remove_if(bricks, [](const Brick &m_brick) { return m_brick.is_destroyed; }).begin(),
-                     std::end(bricks));
-        score = 44 - static_cast<int>(bricks.size());
-        if (ball.begin_phase)
-        {
-            score = 0;
-            bricks.clear();
-            for (int i = 0; i < count_blocks_x; ++i)
+            ball.update(ft_step);
+            paddle.update(ft_step);
+            testCollision(paddle, ball);
+            for (auto &brick: bricks)
             {
-                for (int j = 0; j < count_blocks_y; ++j)
+                testCollision(brick, ball);
+            }
+            bricks.erase(
+                    std::ranges::remove_if(bricks, [](const Brick &m_brick) { return m_brick.is_destroyed; }).begin(),
+                    std::end(bricks));
+            score = 44 - static_cast<int>(bricks.size());
+            if (ball.reset)
+            {
+                ball.velocity.x = 0;
+                ball.shape.setPosition(paddle.x(), paddle.y() - 20.f);
+                score = 0;
+                bricks.clear();
+                for (int i = 0; i < count_blocks_x; ++i)
                 {
-                    bricks.emplace_back((static_cast<float>(i) + 1.f) * (block_width + 3) + 22,
-                                        (static_cast<float>(j) + 2.f) * (block_height + 3));
+                    for (int j = 0; j < count_blocks_y; ++j)
+                    {
+                        bricks.emplace_back((static_cast<float>(i) + 1.f) * (block_width + 3) + 22,
+                                            (static_cast<float>(j) + 2.f) * (block_height + 3));
+                    }
                 }
             }
+            score_text = sf::Text("Score: " + std::to_string(score), myFont, 20);
+            score_text.setFillColor(sf::Color::White);
+            score_text.setPosition(window_width / 2.f, 15.f);
+            score_text.setOrigin(score_text.getGlobalBounds().getSize().x / 2.f,
+                                 score_text.getGlobalBounds().getSize().y / 2.f);
         }
-        window.clear(sf::Color::Black);
-
-        sf::Text score_text("Score: " + std::to_string(score), myFont, 20);
-        score_text.setFillColor(sf::Color::White);
-        score_text.setPosition(window_width / 2.f, 15.f);
-        score_text.setOrigin(score_text.getGlobalBounds().getSize().x / 2.f,
-                             score_text.getGlobalBounds().getSize().y / 2.f);
-
+    }
+    void drawPhase()
+    {
         window.draw(ball.shape);
         window.draw(paddle.shape);
         window.draw(score_text);
         for (auto &brick: bricks)
             window.draw(brick.shape);
         window.display();
-        auto time_point_2 = std::chrono::high_resolution_clock::now();
-
-        auto time_elapsed(time_point_2 - time_point_1);
-
-        FrameTime ft{std::chrono::duration_cast<std::chrono::duration<float, std::milli>>(time_elapsed).count()};
-
-        auto ft_in_seconds(ft / 1000.f);
-        auto fps(1.f / ft_in_seconds);
-
-        window.setTitle("FT: " + std::to_string(ft) + "\tPFS: " + std::to_string(fps));
     }
+};
 
+
+int main()
+{
+    Game{}.run();
     return 0;
 }
